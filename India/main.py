35de5d19ee5b8e128bd267706e558b817803d58a
SUTAM PAUL <102974292+Sutampaul990@users.noreply.github.com>
2022-09-07 23:18:40 +0530

    Add files via upload

---
+++ India/main.py
@@ -0,0 +1,41 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.setup(width=640, height=747)
+screen.title("India States Game")
+
+image = "India_map_blank.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+data = pandas.read_csv("india_states.csv")
+
+all_states = data["state"].to_list()
+guessed_states = []
+missing_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/28 State's correct", prompt="What's another state's "
+                                                                                              "name?").title()
+    if answer_state == "Exit":
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("States_to_learn1.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
